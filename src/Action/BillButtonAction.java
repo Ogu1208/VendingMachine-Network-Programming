20f3f83074321d5ffaecdd4510fc796f92d92cb6
Ogu1208 <kdasunb@naver.com>
2024-06-13 01:20:19 +0900

    feat: 지폐 투입 5000원까지로 제한 구현
    - 입력 받을 수 있는 화폐의 단위는 10원, 50원, 100원, 500원, 1,000원으로만 한정하되, 지폐로 입력 받을 수 있는 금액의 상한선은 5,000원까지이며, 지폐와 동전을 모두 포함하여 총 7,000원을 초과하여 입력 받을 수 없다 (화폐의 입력 금액에 따라 판매 가능 음료가 표기되어야 한다).

--- src/Action/BillButtonAction.java
+++ src/Action/BillButtonAction.java
@@ -13,6 +13,7 @@ public class BillButtonAction implements ActionListener {
 
     private int value;
     private JTextField takeMoneytext;
+    private static int totalBillAmount = 0; // 지폐 투입 총합
 
     public BillButtonAction(int value, JTextField takeMoneytext) {
         this.value = value;
@@ -22,25 +23,30 @@ public class BillButtonAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         int currentMoney = Integer.parseInt(takeMoneytext.getText());
-        if (currentMoney + value <= 7000) {
-            currentMoney += value;
-            takeMoneytext.setText(String.valueOf(currentMoney));
+        if (totalBillAmount + value <= 5000) { // 지폐 투입 상한선 체크
+            if (currentMoney + value <= 7000) { // 총 투입 금액 상한선 체크
+                currentMoney += value;
+                takeMoneytext.setText(String.valueOf(currentMoney));
+                totalBillAmount += value; // 지폐 투입 총합 업데이트
 
-            // 총 매출액 업데이트
-            Admin.setTotalMoney(Admin.getTotalMoney() + value);
-            MachinePanelRight.totalMoneyLabel.setText("총 매출액 : " + Admin.getTotalMoney());
+                // 총 매출액 업데이트
+                Admin.setTotalMoney(Admin.getTotalMoney() + value);
+                MachinePanelRight.totalMoneyLabel.setText("총 매출액 : " + Admin.getTotalMoney());
 
-            // CoinArray에 지폐 추가
-            for (int i = 0; i < CoinArray.coinList.size(); i++) {
-                if (CoinArray.coinList.get(i).getCoinName().equals(String.valueOf(value))) {
-                    CoinArray.coinList.get(i).setCoinNum(CoinArray.coinList.get(i).getCoinNum() + 1);
+                // CoinArray에 지폐 추가
+                for (int i = 0; i < CoinArray.coinList.size(); i++) {
+                    if (CoinArray.coinList.get(i).getCoinName().equals(String.valueOf(value))) {
+                        CoinArray.coinList.get(i).setCoinNum(CoinArray.coinList.get(i).getCoinNum() + 1);
+                    }
                 }
-            }
 
-            // moneyTable 업데이트
-            updateMoneyTable();
+                // moneyTable 업데이트
+                updateMoneyTable();
+            } else {
+                JOptionPane.showMessageDialog(new JFrame(), "총 투입 금액은 7,000원을 초과할 수 없습니다.");
+            }
         } else {
-            JOptionPane.showMessageDialog(new JFrame(), "총 투입 금액은 7,000원을 초과할 수 없습니다.");
+            JOptionPane.showMessageDialog(new JFrame(), "지폐 투입 금액은 5,000원을 초과할 수 없습니다.");
         }
     }
 
